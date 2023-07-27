197b5bb5170b5c3b23fa706b2f1c1b47c468ccc8
thisWeb1225 <shan881225@gmail.com>
2023-07-27 18:39:52 +0800

    :sparkles: Feat: Modify the home page's sturcture of components

---
+++ components/HeroBanner.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className='beats-solo'>SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <Image src="" alt="headphones" className='hero-banner-image'></Image>
+
+        <div>
+          <Link href="/product/ID">
+            <button type="button">BUTTON TEXT</button>
+          </Link>
+          <div className='desc'>
+            <h5>Description</h5>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
