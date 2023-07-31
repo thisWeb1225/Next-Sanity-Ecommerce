f88e1a8e8399f045257bbcaacf5bf15336ff6975
thisWeb1225 <shan881225@gmail.com>
2023-07-31 18:47:32 +0800

    :lipstick: Style: Add Product's image, title, price and description

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
 
-const Product = () => {
+import { urlFor } from '@/lib/client';
+
+import { ProductType } from '@/type/productType';
+
+type ProductPropsType = {
+  product: ProductType
+}
+
+const Product = ({ product: { image, name, slug, price } }: ProductPropsType) => {
+
+  const productImgUrl = urlFor(image && image[0]).url()
+  console.log(productImgUrl)
+
   return (
-    <div>Product</div>
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className='product-card'>
+          <Image src={productImgUrl} alt="product-image" width={250} height={250}></Image>
+          <p className='product-name'>{name}</p>
+          <p className='product-price'>${price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
