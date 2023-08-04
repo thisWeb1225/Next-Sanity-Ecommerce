528c8d73a58742b63d431d3a29a1c2e36b2b33ed
thisWeb1225 <shan881225@gmail.com>
2023-08-04 15:53:33 +0800

    :lipstick: Style: Add Layout component for all page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import Head from "next/head"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
 
-const Layout = () => {
+type LayoutPropsType = {
+  children: React.ReactNode,
+  className: any
+}
+
+const Layout = ({children, className} : LayoutPropsType) => {
   return (
-    <div>Layout</div>
+    <div className={`layout ${className}`}>
+      <Head>
+        <title>Speak Sells</title>
+      </Head>
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
   )
 }
 
