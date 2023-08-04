ee29e6d7d492075599de5b11aedc16e0b773a153
thisWeb1225 <shan881225@gmail.com>
2023-08-04 15:53:09 +0800

    :lipstick: Style: Add footer and footer banner

--- components/FooterBanner.tsx
+++ components/FooterBanner.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
 
-const FooterBanner = () => {
+import { footerBannerType } from "@/type/footerBannerType"
+import { urlFor } from "@/lib/client"
+
+type FooterBannerPropsType = {
+  footerBanner: footerBannerType
+}
+
+// {footerBanner} : FooterBannerPropsType
+
+const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, product, buttonText, image, desc } }: FooterBannerPropsType) => {
+
+  const footerImgUrl = urlFor(image).width(555).url()
+
   return (
-    <div>FooterBanner</div>
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+        </div>
+      </div>
+
+      <Image 
+        src={footerImgUrl}
+        className="footer-banner-image"
+        alt="footer banner"
+        width={555}
+        height={555}
+        priority={true}
+      />
+    </div>
   )
 }
 
