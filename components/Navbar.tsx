005ad0bfadf7aabaea80a3301684c125bfa0a146
thisWeb1225 <shan881225@gmail.com>
2023-08-04 15:53:48 +0800

    :lipstick: Style: Add navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import Link from "next/link";
+import { AiOutlineShopping } from 'react-icons/ai'
+
+import Cart from "./Cart";
+import { useProductStateContext } from "@/context/ProductStateProvider";
+import { useCartStateContext } from "@/context/CartStateProvider";
 
 const Navbar = () => {
+
+  const {showCart, setShowCart, totalQuantities} = useCartStateContext()
+
   return (
-    <div>Navbar</div>
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href='/'>HeadPhones</Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+        <AiOutlineShopping></AiOutlineShopping>
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
+
+      {showCart && <Cart/>}
+    </div>
   )
 }
 
