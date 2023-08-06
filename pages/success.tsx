45dadc2d55556cbd6eb8f3361a46febeb1048576
thisWeb1225 <shan881225@gmail.com>
2023-08-06 21:12:47 +0800

    :sparkles: Feat: Add success page when checkout success

---
+++ pages/success.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react'
+import Link from 'next/link'
+import { BsBagCheckFill } from 'react-icons/bs'
+import { useRouter } from 'next/router'
+
+import { useCartStateContext } from '@/context/CartStateProvider'
+
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useCartStateContext();
+  
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+  })
+
+  return (
+    <div className='success-wrapper'>
+      <div className='success'>
+        <p className='icon'>
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className='email-msg'>Check your email inbox for the receipt</p>
+        <p className='description'>
+          If you have any questition, please email
+          <a className='email' href="#">
+            order@example.com
+          </a>
+        </p>
+        <Link href={'/'}>
+          <button type="button" className='btn'>Continue Shopping</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Success
